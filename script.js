086f969f7fab6f51b3037b6990c349563c009d9d
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2024-11-01 02:39:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function reserva(){
+    let nome = document.getElementById('nome').value;
+
+    alert('Parabéns ' + nome + '! Sua reserva foi concluída. Verifique mais informações na caixa de entrada do seu E-mail.');
+}
